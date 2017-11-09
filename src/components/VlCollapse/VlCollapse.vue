b7cb7c5970b9bedd272a09493817813d82c462d1
tjlastnumber <www.514163307@qq.com>
2017-11-09 17:16:30 +0800

    add collapse components

---
+++ src/components/VlCollapse/VlCollapse.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <span class="vl-collapse-title" @click="priIsOpen = !priIsOpen"> {{title}} </span>
+    <div v-show="priIsOpen">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VlCollapse',
+  model: {
+    prop: 'isOpen',
+    event: 'openChanged'
+  },
+  props: {
+    title: { type: String },
+    isOpen: { type: Boolean, default: false }
+  },
+  data () {
+    return {
+      priIsOpen: false
+    }
+  },
+  methods: {
+    onOpenChanged (val) {
+      this.$emit('openChanged', val)
+    }
+  },
+  watch: {
+    isOpen (val) {
+      this.priIsOpen = val
+      this.onOpenChanged(val)
+    }
+  }
+}
+</script>
+
+<style>
+  .vl-collapse-title {
+    cursor: pointer;
+  }
+</style>
+
