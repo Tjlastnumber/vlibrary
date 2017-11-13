399cdda6746ef4d4ff9ce71d7d78364c767ab60e
tjlastnumber <www.514163307@qq.com>
2017-11-13 09:37:01 +0800

    分离css文件

--- src/components/VlCollapse/VlCollapse.vue
+++ src/components/VlCollapse/VlCollapse.vue
@@ -3,7 +3,7 @@
     <div class="vl-collapse-header" @click="priIsOpen = !priIsOpen">
       <span> {{title}} </span>
       <span class="flex"></span>
-      <span class="vl-collapse-icon" :class="priIsOpen ? 'glyphicon glyphicon-menu-down' : 'glyphicon glyphicon-menu-right'"></span>
+      <span class="vl-collapse-icon" :class="priIsOpen ? 'glyphicon glyphicon-menu-up' : 'glyphicon glyphicon-menu-down'"></span>
     </div>
     <div v-show="priIsOpen">
       <slot></slot>
@@ -11,6 +11,7 @@
   </div>
 </template>
 
+<style src="./VlCollapse.css"></style>
 <script>
 export default {
   name: 'VlCollapse',
@@ -40,39 +41,3 @@ export default {
   }
 }
 </script>
-
-<style>
-  .vl-collapse-header {
-    cursor: pointer;
-    border-radius: 0;
-    /* cursor: pointer; */
-    display: flex;
-    -webkit-box-align: inherit;
-    -webkit-align-items: inherit;
-    align-items: inherit;
-    line-height: 40px;
-    margin: 0;
-    max-height: 40px;
-    overflow: hidden;
-    padding: 0px 16px;
-    text-align: left;
-    text-decoration: none;
-    white-space: normal;
-    width: 100%;
-  }
-
-  .vl-collapse-header:hover {
-    background-color: rgba(230, 230, 230, 0.2);
-  }
-
-  .flex {
-    flex: 1;
-    -webkit-box-flex: 1;
-    min-width: 0;
-  }
-
-  .vl-collapse-icon {
-    line-height: 40px;
-  }
-</style>
-
