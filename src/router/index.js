1cc0136705086c3afe557f8b6b6f99c5591555b2
tjlastnumber <www.514163307@qq.com>
2017-11-13 16:09:54 +0800

    update route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '@/page/HelloWorld'
+import TablePage from '@/page/TablePage'
 
 Vue.use(Router)
 
@@ -10,6 +11,10 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: HelloWorld
+    }, {
+      path: '/table',
+      name: 'Table',
+      component: TablePage
     }
   ]
 })
