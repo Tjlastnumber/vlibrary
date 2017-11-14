1665ea9e059a408935fe8f82f38e7e76040ac485
tjlastnumber <www.514163307@qq.com>
2017-11-14 10:01:55 +0800

    update route link
    Signed-off-by: tjlastnumber <www.514163307@qq.com>

--- src/Layout.vue
+++ src/Layout.vue
@@ -15,20 +15,12 @@
           <div class="vl-sidenav-content">
             <vl-collapse title="Group1" style="color: white">
               <ul class="menu-list menu-toggle-list">
-                <li v-for="(item, index) in navRouter" :key="index">
-                  <a href="#">
-                    {{ item.title }}
-                  </a>
-                </li>
-              </ul>
-            </vl-collapse>
-
-            <vl-collapse title="Group1" style="color: white">
-              <ul class="menu-list menu-toggle-list">
-                <li v-for="(item, index) in navRouter" :key="index">
-                  <a href="#">
-                    {{ item.title }}
-                  </a>
+                <li>
+                  <router-link v-for="(route,index) in $router.options.routes"
+                               :key="index"
+                               :to="route.path">
+                    {{ route.name }}
+                  </router-link>
                 </li>
               </ul>
             </vl-collapse>
@@ -39,37 +31,30 @@
       </vl-side-layout>
 
       <vl-content-layout>
+
         <vl-toolbar>
-          <vl-shadow dp="1">
+          <vl-shadow :dp="1">
             <vl-toolbar-sub v-show="!isOpenSidenav">
               <button @click="isOpenSidenav = !isOpenSidenav">
                 <span class="glyphicon glyphicon-menu-hamburger"></span>
               </button>
             </vl-toolbar-sub>
             <vl-toolbar-sub>
-              <h3> {{ title }} </h3>
+              <h3> {{ $route.name }} </h3>
             </vl-toolbar-sub>
-            <vl-toolbar-sub>
+            <span class="flex"></span>
+            <vl-toolbar-sub style="margin-right: 8px">
               <button @click="isOpenSetting = !isOpenSetting">
                 <span class="glyphicon glyphicon-cog"></span>
               </button>
             </vl-toolbar-sub>
           </vl-shadow>
         </vl-toolbar>
+
         <div class="vl-content">
           <div class="container-fluid">
             <div class="row">
               <div class="col-12">
-                <vl-collapse title="Group1">
-                  <ul>
-                    <li v-for="(item, index) in navRouter" :key="index">
-                      <a href="#">
-                        {{ item.title }}
-                      </a>
-                    </li>
-                  </ul>
-                </vl-collapse>
-
                 <!-- main content -->
                 <slot></slot>
 
@@ -81,16 +66,23 @@
 
       <!-- Setting -->
       <vl-side-layout>
-        <vl-sidenav class="setting" :dp="4" :is-open="isOpenSetting" :dock="'right'" :absolute="true">
-          <vl-shadow dp="1">
-            <vl-toolbar  class="sidenav-background">
+        <vl-sidenav class="setting" :dp="5" :is-open="isOpenSetting" :dock="'right'" :absolute="true">
+          <vl-shadow :dp="1">
+            <vl-toolbar>
               <vl-toolbar-sub>
                 <h3> Setting </h3>
+              </vl-toolbar-sub>
+              <span class="flex"></span>
+              <vl-toolbar-sub style="margin-right: 8px">
+                <button @click="isOpenSetting = !isOpenSetting">
+                  <span class="glyphicon glyphicon-remove"></span>
+                </button>
               </vl-toolbar-sub>
             </vl-toolbar>
           </vl-shadow>
         </vl-sidenav>
       </vl-side-layout>
+
     </vl-layout>
   </div>
 </template>
@@ -122,142 +114,20 @@
     },
     data () {
       return {
-        title: 'Toolbar',
         isOpenSidenav: true,
-        isOpenSetting: false,
-        navRouter: [
-          {
-            title: '1'
-          },
-          {
-            title: '2'
-          },
-          {
-            title: '3'
-          },
-          {
-            title: '4'
-          },
-          {
-            title: '5'
-          },
-          {
-            title: '6'
-          }
-        ]
+        isOpenSetting: false
       }
     },
+    mounted () {
+      console.info(this.$router.options.routes)
+    },
+    // watch: {
+    //   '$route' () {
+    //     this.title = this.$route.name
+    //   }
+    // },
     methods: {}
   }
 </script>
 
-<style>
-  .sidenav-content {
-    padding: 16px;
-  }
-
-  .vl-content {
-    padding: 16px;
-  }
-
-  .vl-sidenav-content {
-    display: block;
-    position: relative;
-    overflow: auto;
-    -webkit-overflow-scrolling: touch;
-  }
-
-  html, body {
-    height: 100%;
-    margin: 0px;
-    max-height: 100%;
-    max-width: 100%;
-    min-height: 0px;
-    overflow-x: hidden;
-    overflow-y: hidden;
-    padding: 0px;
-    position: relative;
-    width: 100%;
-  }
-
-  a.nav-log {
-    text-decoration: none;
-  }
-
-  .nav-log {
-    text-align: center;
-    display: -webkit-box;
-    display: -webkit-flex;
-    display: flex;
-    -webkit-box-orient: vertical;
-    -webkit-box-direction: normal;
-    -webkit-flex-direction: column;
-    flex-direction: column;
-    -webkit-box-align: center;
-    -webkit-align-items: center;
-    align-items: center;
-    padding: 0 0 16px;
-    margin: 0 auto;
-  }
-
-  .nav-log > img {
-    height: 150px;
-    width: auto;
-    display: block;
-    -webkit-transform-origin: 50% 0;
-    transform-origin: 50% 0;
-    -webkit-transform: scale(1.2);
-    transform: scale(1.2);
-  }
-
-  .sidenav-header {
-    background-color: #41b883;
-    background: -webkit-linear-gradient(#36495d, #41b883);
-    background: linear-gradient(#36495d, #41b883);
-    border-bottom: 1px solid #41b883;
-    -webkit-flex-shrink: 0;
-    flex-shrink: 0;
-    z-index: 2;
-  }
-
-  .sidenav-background {
-    background-color: #41b883;
-  }
-
-  .setting {
-    background-color: white;
-  }
-
-  .menu-toggle-list a {
-    overflow: hidden;
-    position: relative;
-    z-index: 1;
-    display: block;
-    padding: 0 16px 0 32px;
-    text-transform: none;
-    text-rendering: optimizeLegibility;
-    border-radius: 0;
-    color: white;
-    cursor: pointer;
-    display: block;
-    -webkit-box-align: inherit;
-    -webkit-align-items: inherit;
-    align-items: inherit;
-    line-height: 40px;
-    margin: 0;
-    max-height: 40px;
-    overflow: hidden;
-    text-align: left;
-    text-decoration: none;
-    white-space: normal;
-    width: 100%;
-    font-weight: 500;
-    /* font-size: 14px; */
-    font-style: inherit;
-  }
-
-  .menu-toggle-list a:hover {
-    background-color: rgba(230, 230, 230, 0.2);
-  }
-
-</style>
+<style src="./main.css"></style>
---
+++ src/components/VlCollapse/VlCollapseHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <span class="vl-collapse-title" @click="priIsOpen = !priIsOpen"> {{title}} </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VlCollapseHeader'
+}
+</script>
+
+
+<style>
+
+</style>
+
