e90231289110cde429c68afae87d09d9866b7005
tjlastnumber <www.514163307@qq.com>
2017-11-15 18:02:26 +0800

    sidenav add animate

--- src/components/VlSidenav/VlSidenav.vue
+++ src/components/VlSidenav/VlSidenav.vue
@@ -1,13 +1,19 @@
 <template>
-  <vl-shadow :dp="dp" class="vl-sidenav" :class="dynamicClass" v-show="isOpen">
-    <slot></slot>
-  </vl-shadow>
+  <transition @beforeEnter="beforeEnter"
+              @enter="enter"
+              @leave="leave">
+    <vl-shadow ref="content" :dp="dp" class="vl-sidenav" :class="dynamicClass">
+      <slot></slot>
+    </vl-shadow>
+  </transition>
 </template>
 
 <style src="./VlSidenav.css"></style>
 
 <script>
+  import Velocity from 'velocity'
   import VlShadow from '../VlShadow/VlShadow.vue'
+
   export default {
     name: 'VlSidenav',
     components: { VlShadow },
@@ -62,11 +68,25 @@
       },
       setMask (val) {
         val ? this.mask.show() : this.mask.close()
+      },
+      beforeEnter (el) {
+
+      },
+      enter (el, done) {
+        let translate = { 'min-width': '272px', translateX: 0 }
+        // { translateX: 0 }
+        Velocity(el, translate, { duration: 200 })
+      },
+      leave (el, done) {
+        let translate = { 'min-width': 0, width: 0, translateX: -272 }
+        Velocity(el, translate, { duration: 200 })
       }
     },
     watch: {
       isOpen (val) {
         this.setMask(val && !this.compareWidth())
+        let el = this.$refs['content'].$el
+        val ? this.enter(el) : this.leave(el)
       }
     }
   }
