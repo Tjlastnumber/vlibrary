bb78e33f510cd9a28cbb3d764bcb375db99f7584
tjlastnumber <www.514163307@qq.com>
2017-11-15 15:56:54 +0800

    collapse component add animate

--- src/components/VlCollapse/VlCollapse.vue
+++ src/components/VlCollapse/VlCollapse.vue
@@ -1,18 +1,27 @@
 <template>
   <div>
-    <div class="vl-collapse-header" @click="priIsOpen = !priIsOpen">
+    <div class="vl-collapse-header" @click="click">
       <span> {{title}} </span>
       <span class="flex"></span>
-      <span class="vl-collapse-icon" :class="priIsOpen ? 'glyphicon glyphicon-menu-up' : 'glyphicon glyphicon-menu-down'"></span>
+      <span ref="icon" class="vl-collapse-icon" :class="'glyphicon glyphicon-menu-down'"></span>
     </div>
-    <div v-show="priIsOpen">
-      <slot></slot>
-    </div>
+    <transition @beforeEnter="beforeEnter"
+                @enter="enter"
+                @afterEnter="afterEnter"
+                @beforeLeave="beforeLeave"
+                @leave="leave"
+                @afterLeave="afterLeave">
+      <div v-show="priIsOpen">
+        <slot></slot>
+      </div>
+    </transition>
   </div>
 </template>
 
 <style src="./VlCollapse.css"></style>
+
 <script>
+import Velocity from 'velocity'
 export default {
   name: 'VlCollapse',
   model: {
@@ -25,18 +34,60 @@ export default {
   },
   data () {
     return {
-      priIsOpen: false
+      priIsOpen: this.isOpen,
+      beforeToggleStyle: {}
     }
   },
+  mounted () {
+    this.retateIcon()
+  },
   methods: {
+    click () {
+      this.priIsOpen = !this.priIsOpen
+      this.retateIcon()
+      this.onOpenChanged(this.priIsOpen)
+    },
     onOpenChanged (val) {
       this.$emit('openChanged', val)
+    },
+    retateIcon () {
+      let elIcon = this.$refs['icon']
+      let retateZ = (this.priIsOpen ? '-180' : '0') + 'deg'
+      Velocity(elIcon, { rotateZ: retateZ })
+    },
+    beforeEnter (el) {
+      this.$set(this.beforeToggleStyle, 'oldOverflow', this.$el.style.overflow)
+    },
+    enter (el, done) {
+      el.style.height = 0
+      this.$el.style.overflow = 'hidden'
+      Velocity(el, {
+        height: el.scrollHeight + 'px'
+      }, {
+        complete: done
+      })
+    },
+    afterEnter (el) {
+      this.$el.style.overflow = this.beforeToggleStyle.oldOverflow
+    },
+    beforeLeave (el) {
+      this.beforeToggleStyle.oldOverflow = this.$el.style.overflow
+    },
+    leave (el, done) {
+      this.$el.style.overflow = 'hidden'
+      Velocity(el, {
+        height: 0
+      }, {
+        complete: done
+      })
+    },
+    afterLeave (el) {
+      this.$el.style.overflow = this.beforeToggleStyle.oldOverflow
     }
   },
   watch: {
     isOpen (val) {
       this.priIsOpen = val
-      this.onOpenChanged(val)
     }
   }
 }
