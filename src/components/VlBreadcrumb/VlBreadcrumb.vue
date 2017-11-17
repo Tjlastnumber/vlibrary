c339f5f41b9bc21b3f9bfa9c6165c39acb6cf139
tjlastnumber <www.514163307@qq.com>
2017-11-17 11:10:48 +0800

    Breadcrumb update animate

--- src/components/VlBreadcrumb/VlBreadcrumb.vue
+++ src/components/VlBreadcrumb/VlBreadcrumb.vue
@@ -1,9 +1,37 @@
 <template>
-
+  <div ref="content">
+    <slot></slot>
+  </div>
 </template>
 
 <script>
+import Velocity from 'velocity'
 export default {
-  name: 'VlBreadcrumb'
+  name: 'VlBreadcrumb',
+  props: {
+    path: {
+      type: String
+    }
+  },
+  watch: {
+    path (val) {
+      this.$refs['content'].style.opacity = 0
+      Velocity(this.$refs['content'], {
+        opacity: 1
+      })
+    }
+  }
 }
 </script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity .3s ease;
+}
+
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+</style>
+
